311bf981dbcbd8fd95a0ee05a9b7c2d3e8e9e36b
lucas-abc <lucas.abc@protonmail.com>
2022-12-13 23:38:34 -0300

    update routes and add data

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 // CONFIG
 const __filename = fileURLToPath(import.meta.url);
@@ -44,6 +50,7 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 // ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
@@ -52,4 +59,8 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
   }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    User.insertMany(users);
+    Post.insertMany(posts);
+
   }).catch((error) => console.log(`${error} did not connect`));
